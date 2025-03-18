3746838ed9374f4a861214568c1b3c60db697432
Peace Ishimwe <peaceishimwem@gmail.com>
2025-03-18 16:56:57 +0200

    feat: model training

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+
 import shutil
 import os
 from scripts.process import process_image  # Import processing function
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)  # Ensure the directory exists
 
-@app.post("/uploads/")
+@app.post("/uploads")
 async def upload_image(file: UploadFile = File(...)):
     # Save uploaded image
     file_path = os.path.join(UPLOAD_DIR, file.filename)
--- static/demo.js
+++ static/demo.js
@@ -1,15 +1,19 @@
 async function uploadImage() {
-    let fileInput = document.getElementById("fileInput");
-    let formData = new FormData();
-    formData.append("file", fileInput.files[0]);
+    try {
+        let fileInput = document.getElementById("fileInput");
+        let formData = new FormData();
+        formData.append("file", fileInput.files[0]);
 
-    let response = await fetch("http://127.0.0.1:8000/uploads/", {
-        method: "POST",
-        body: formData
-    });
+        let response = await fetch("http://127.0.0.1:8000/uploads", {
+            method: "POST",
+            body: formData
+        });
 
-    let data = await response.json();
-    document.getElementById("result").innerText =
-        `Rwandan ID: ${data.is_rwandan_id ? "Confirmed" : "Not Confirmed"}
+        let data = await response.json();
+        document.getElementById("result").innerText =
+            `Rwandan ID: ${data.is_rwandan_id ? "Confirmed" : "Not Confirmed"}
         \nExtracted Text: ${data.extracted_texts.join(", ")}`;
+    } catch (error) {
+        console.log(error)
+    }
 }
